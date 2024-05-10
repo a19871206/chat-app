544bb55d468b9e120116c0e07e9be7a6661fcb93
watanabe-tsuguharu <a19871206@gmail.com>
2024-05-10 11:10:06 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
