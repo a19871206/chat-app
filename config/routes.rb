4325eb6d9a2869bd875e4964ef54bbe2a5ee0818
watanabe-tsuguharu <a19871206@gmail.com>
2024-05-10 13:39:05 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
